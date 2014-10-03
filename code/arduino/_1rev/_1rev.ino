414c1354aae438a30430b73f8e627ee424a3667c
Francisco Sanchez Arroyo <hola@fablabsitges.org>
2014-10-03 14:28:38 +0200

    John Rees is back

--- code/arduino/_1rev/_1rev.ino
+++ code/arduino/_1rev/_1rev.ino
@@ -2,11 +2,18 @@
 
 // Steppers 1, 2 and 4 on big easy driver, stepper 3 on easydriver
 
+String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.:e ";
 
 AccelStepper stepper1(1,12,13);
 AccelStepper stepper2(1,10,11);
 AccelStepper stepper3(1,6,7);
 AccelStepper stepper4(1,8,9);
+
+boolean calibrated = false;
+
+char serialdata[4];
+    int lf = 10;
+
 void setup()
 {  
    Serial.begin(9600);
@@ -16,53 +23,44 @@ void setup()
   }
   
   Serial.write("Flip-Flap Ready!");
+      Serial.println("Enter the 4 characters that the flipflap is displaying:");
+
    stepper1.setMaxSpeed(12000);
    stepper1.setAcceleration(24000.0);
    
    stepper2.setMaxSpeed(12000);
-   stepper2.setAcceleration(12000.0);
+   stepper2.setAcceleration(24000.0);
    
-   stepper3.setMaxSpeed(4000);
-   stepper3.setAcceleration(3000.0);
+   stepper3.setMaxSpeed(12000);
+   stepper3.setAcceleration(24000.0);
 
    stepper4.setMaxSpeed(12000);
-   stepper4.setAcceleration(12000.0);   
+   stepper4.setAcceleration(24000.0);   
 }
 void loop()
 {  
    if (Serial.available() > 0) {
-    // read incoming serial data:
-    char inChar = Serial.read();
-    // Type the next ASCII value from what you received:
-     switch (inChar){
-     case 'a':
-     stepper1.runToNewPosition(480);
-     stepper1.setCurrentPosition(0);
-     break;
-     case 'b':
-     stepper2.runToNewPosition(480);
-     stepper2.setCurrentPosition(0);
-     break;
-     case 'c':
-     stepper3.runToNewPosition(240);
-     stepper3.setCurrentPosition(0);
-     break;
-     case 'd':
-     stepper4.runToNewPosition(480);
-     stepper4.setCurrentPosition(0);
-     break;
-     }
+
+    Serial.readBytesUntil(lf, serialdata, 4);
+    
+    char inChar = serialdata[3];
+    int p = s.indexOf(inChar);
+
+    if (calibrated) {
+      // read incoming serial data:
+      if (p >= 0) {
+        stepper1.runToNewPosition(480 * p);
+        stepper1.setCurrentPosition(0);
+        s = s.substring(p) + s.substring(0,p);
+      } else if (inChar == '`') {
+        stepper1.runToNewPosition(480);
+        stepper1.setCurrentPosition(0);
+      }
+    } else {
+      s = s.substring(p) + s.substring(0,p);
+      calibrated = true;
+    }
+    
   }  
-  
-  
- //  stepper1.runToNewPosition(9600); //240 steps per letter
- //  stepper2.runToNewPosition(9600);
- //  stepper3.runToNewPosition(-4800); //120 steps per letter
- //  stepper4.runToNewPosition(9600);
- //  stepper1.setCurrentPosition(0);
- //  stepper2.setCurrentPosition(0);
- //  stepper3.setCurrentPosition(0);
- //  stepper4.setCurrentPosition(0);
- //  delay(2000);
 
 }
