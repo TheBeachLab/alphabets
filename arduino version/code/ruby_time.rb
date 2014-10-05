d12ca2303a320818d7bfab47d0f47a1c2935fc7f
John Rees <john@bitsushi.com>
2014-10-05 12:57:44 +0200

    ruby time updates

--- arduino version/code/ruby_time.rb
+++ arduino version/code/ruby_time.rb
@@ -1,50 +1,30 @@
 require 'serialport'
+
 port_str = '/dev/tty.usbmodem1421'
+initial_message = "ITS "
+
+# do not edit below this line
 baud_rate = 9600
 data_bits = 8
 stop_bits = 1
 parity = SerialPort::NONE
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
-sp.write("ITS \n")
-# sleep(2)
-# sp.write("1234\n")
-
 def write(sp,word)
 	sp.write("#{word}\n")
 end
 
+write(sp, initial_message)
+
 while true
-	sp.write('TICK')
+	write(sp,'TICK')
 	sleep(2.8)
-	sp.write('TOCK')
+	write(sp,'TOCK')
 	sleep(2.3)
-	sp.write('ITS ')
+	write(sp,'ITS ')
 	sleep(3)
-	sp.write("#{Time.now.strftime('%H%M')}\n")
+	write(sp,"#{Time.now.strftime('%H%M')}\n")
 	sleep(6)
-	sp.write('    ')
+	write(sp,'    ')
 	sleep(2.5)
 end
-
-# # sp.write("    \n")
-# # sleep(1)
-# # write(sp, "    ")
-# # sleep(4)
-
-# while true
-# 	['CIAO', 'ROMA', 'MAKE', 'YOUR', 'OWN ', 'FLIP', 'FLAP', ' OR ', 'BUY ', 'ONE ', 'BOX ', 'FOR ', 'JUST', 'e49 ', 'FROM', 'OUR ', 'WEB ', 'SITE', '    '].each do |w|
-# 		s = ""
-# 		write(sp,w)
-# 		while true
-# 			while (i = sp.gets.chomp) do
-# 				s += i
-# 				puts s
-# 				break if s == "!!"
-# 			end
-# 			break
-# 		end
-		
-# 		sleep(2)
-# 	end
-# end
